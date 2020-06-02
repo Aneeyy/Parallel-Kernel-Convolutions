9a30d98dbbd6527c9f1e3d3a0a4c7a6a28d2113e
root <root@DESKTOP-RV970CI.localdomain>
2020-06-02 15:54:40 -0700

    started openMP convolution

--- OpenMP/kernelConvolution.c
+++ OpenMP/kernelConvolution.c
@@ -85,16 +85,79 @@ double ** getKernel(cJSON* config, int *kernelSize){
 
     }
 
+    // Blurring kernel testing
+    for (int i = 0; i < ks; i++){
+        for (int j = 0; j < ks; j++){
+            kernel[i][j] = 1.0/9.0;
+        }
+    }
 
-
-
-    kernel[0][0] = 0.0;
-    kernel[ks-1][ks-1] = 1.0;
+    // kernel[0][0] = 0.0;
+    // kernel[ks-1][ks-1] = 1.0;
 
     return kernel;
 
 
+}
 
+void performConv(char* fileInputLocation,char* fileOutputLocation,double** kernel,int kernelSize){
+    FILE *source, *dest;
+    source = fopen(fileInputLocation, "r");
+    dest = fopen(fileOutputLocation, "w+");
+
+    if( source == NULL ) {
+
+        return;
+    } 
+
+    unsigned char byte[54];
+
+    for(int i=0;i<54;i++){									
+		byte[i] = getc(source);								
+	}
+    fwrite(byte,sizeof(unsigned char),54,dest);	
+
+    int height = *(int*)&byte[18];
+	int width = *(int*)&byte[22];
+
+    int size = height*width;
+
+    unsigned char (*buff)[3] = malloc(sizeof(unsigned char[size][3]));
+	unsigned char (*out)[3] = malloc(sizeof(unsigned char[size][3]));
+
+    for(int i=0;i<size;i++){
+        buff[i][2]=getc(source);					
+		buff[i][1]=getc(source);
+		buff[i][0]=getc(source);
+	}
+    for(int x=1;x<height-1;x++){					
+		for(int y=1;y<width-1;y++){
+			float sum0= 0.0;
+			float sum1= 0.0;
+			float sum2= 0.0;
+			for(int i=-1;i<=1;++i){
+				for(int j=-1;j<=1;++j){	
+					sum0=sum0+(float)kernel[i+1][j+1]*buff[(x+i)*width+(y+j)][0];
+					sum1=sum1+(float)kernel[i+1][j+1]*buff[(x+i)*width+(y+j)][1];
+					sum2=sum2+(float)kernel[i+1][j+1]*buff[(x+i)*width+(y+j)][2];
+				}
+			}
+			out[(x)*width+(y)][0]=sum0;
+			out[(x)*width+(y)][1]=sum1;
+			out[(x)*width+(y)][2]=sum2;
+		}
+	}
+
+    for(int i=0;i<size;i++){
+		putc(out[i][2],dest);
+		putc(out[i][1],dest);
+		putc(out[i][0],dest);
+
+	}
+    fclose(source);
+    fclose(dest);
+    free(buff);
+    free(out);
 }
 
 int main() {
@@ -128,7 +191,10 @@ int main() {
     //This copy file function is in place of the filter that has to be written
     //In the implementation, read image from the input file, and write image to output file
     //then replace the 100.02 with the seconds it took to perform the kernel convolution
-    copyFile(fileInputLocation,fileOutputLocation);
+    // copyFile(fileInputLocation,fileOutputLocation);
+    
+
+    performConv(fileInputLocation,fileOutputLocation,kernel,kernelSize);
 
 //    double[][] kernel
 //    double kSize = 3;//5
@@ -137,3 +203,5 @@ int main() {
     writeToTimingJSON(100.02, fileOutputLocation);
     return 0;
 }
+
+
